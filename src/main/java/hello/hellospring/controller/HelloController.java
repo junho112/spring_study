629164e6f599ed707e01e6ac9e6bd30364785733
junho112 <byon26@naver.com>
2023-01-01 20:40:59 +0900

    spring study

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,45 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello";
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello-temlpate";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody   //http body부분에 직접 값을 넣어주겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name ; //
+    }
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello(); //커맨드 시프트 엔터 --> 자동완성
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{ //컨트롤 엔터
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name = name;
+        }
+    }
+
+}
